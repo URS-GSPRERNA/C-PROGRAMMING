2edb9fa8eb9e8ab71b6b066885c28326accb28f2
URS-GSPRERNA <85802031+URS-GSPRERNA@users.noreply.github.com>
2021-06-13 00:12:03 +0530

    Create Learning Printf Scanf.c

---
+++ Basics/Learning Printf Scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+     int num1;
+     printf("Amount 1:");
+     scanf("%d", &num1);
+     int num2;
+     printf("Amount 2:");
+     scanf("%d", &num2);
+    printf(" Total amount: %d", num1 + num2 );
+  
+    return 0;
+}
